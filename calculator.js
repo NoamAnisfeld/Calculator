d147dccf8fa4ada6e11475ff19b98647943fde99
Noam Anisfeld <NoamAnisfeld@gmail.com>
2022-04-28 00:33:04 +0300

    Adding button actions

--- calculator.js
+++ calculator.js
@@ -1,6 +1,10 @@
+const DEBUG = true;
+
 document.documentElement.addEventListener('keypress', (event) => {
-    if (event.key === 'z' || event.key === 'Z') {
-        location.reload(true /* bypass catch on Firefox */);
+    if (DEBUG) {
+        if (event.key === 'z' || event.key === 'Z') {
+            location.reload(true /* bypass catch on Firefox */);
+        }
     }
 
     if (event.key === 'Enter') {
@@ -8,20 +12,12 @@ document.documentElement.addEventListener('keypress', (event) => {
     }
 })
 
-function add(a, b) {
-    return a + b;
-}
-
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
+const actions = {
+    add: (a, b) => a + b,    
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b,
+    power: (a, b) => Math.pow(a, b)
 }
 
 function calculate() {
@@ -29,8 +25,8 @@ function calculate() {
         a = Number(document.getElementById('argument1').value),
         b = Number(document.getElementById('argument2').value);
 
-    return add(a, b);
-}    
+    return actions.add(a, b);
+}
 
 function output(str) {
     document.getElementById('output').textContent = str;
@@ -39,5 +35,16 @@ function output(str) {
 document.getElementById('argument1').focus();
 
 document.getElementById('calculate').addEventListener('click', () => {
-    output(`Sum is: ${calculate().toFixed(2)}`);
+    output(`Sum is: ${calculate()}`);
 });
+
+document.getElementById('calculator-keys').addEventListener('click', (event) => {
+    let
+        a = Number(document.getElementById('argument1').value),
+        b = Number(document.getElementById('argument2').value),
+        action = event.target.dataset.action;
+
+    if (action) {
+        actions[action](a, b);
+    }
+})
