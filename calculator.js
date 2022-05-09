72db60fb6ae4decacf30cf5c6fbd1b7c26752139
Noam Anisfeld <NoamAnisfeld@gmail.com>
2022-05-09 12:37:34 +0300

    reworking UI

--- calculator.js
+++ calculator.js
@@ -8,28 +8,40 @@ if (DEBUG) {
         }
     });
 }
-;
 function main() {
-    const tokenizer = new CalculatorEngine;
-    let inputElement = document.getElementById('calculator__input');
-    let outputElement = document.getElementById('calculator__result');
-    if (inputElement === null) {
+    const calculatorEngine = new CalculatorEngine, calculatorUI = document.querySelector(".calculator-wrapper"), inputElement = calculatorUI?.querySelector('input[type="text"]');
+    if (!calculatorUI || !inputElement) {
         return;
     }
-    inputElement.focus();
-    inputElement.addEventListener('keypress', event => {
-        if (outputElement === null) {
-            return;
-        }
-        try {
-            tokenizer.serialize(event.key);
-            console.log(tokenizer.getCurrentStringValue());
-            outputElement.textContent = tokenizer.getCurrentStringValue();
-        }
-        catch (err) {
-            console.error(err);
-            outputElement.innerHTML = `<span style="background:red;">${err}</span>`;
+    calculatorUI.addEventListener("click", (event) => {
+        const target = event.target, dataset = target instanceof HTMLElement ? target.dataset : null, key = dataset?.key;
+        if (key) {
+            calculatorEngine.serialize(key);
+            inputElement.value = calculatorEngine.getCurrentStringValue();
         }
     });
+    //     calculatorUI.addEventListener("keypress", (event) => {
+    //         event.preventDefault();
+    //         inputElement.value = event.key;
+    //     });
+    //     let inputElement = document.getElementById('calculator__input');
+    //     let outputElement = document.getElementById('calculator__result');
+    //     if (inputElement === null) {
+    //         return;
+    //     }
+    //     inputElement.focus();
+    //     inputElement.addEventListener('keypress', event => {
+    //         if (outputElement === null) {
+    //             return;
+    //         }
+    //         try {
+    //             calculatorEngine.serialize(event.key);
+    //             console.log(calculatorEngine.getCurrentStringValue());
+    //             outputElement.textContent = calculatorEngine.getCurrentStringValue();
+    //         } catch (err) {
+    //             console.error(err);
+    //             outputElement.innerHTML = `<span style="background:red;">${err}</span>`;
+    //         }
+    //     });
 }
 main();
--- calculator.ts
+++ calculator.ts
@@ -9,31 +9,55 @@ if (DEBUG) {
             location.reload(true /* bypass catch on Firefox */);
         }
     });
-};
+}
 
 function main(): void {
-    const tokenizer = new CalculatorEngine;
+    const calculatorEngine = new CalculatorEngine,
+        calculatorUI = document.querySelector(".calculator-wrapper"),
+	    inputElement:HTMLInputElement | null | undefined = 
+            calculatorUI?.querySelector('input[type="text"]');
 
-    let inputElement = document.getElementById('calculator__input');
-    let outputElement = document.getElementById('calculator__result');
-
-    if (inputElement === null) {
+    if (!calculatorUI || !inputElement) {
         return;
     }
-    inputElement.focus();
-    inputElement.addEventListener('keypress', event => {
-        if (outputElement === null) {
-            return;
-        }
-        try {
-            tokenizer.serialize(event.key);
-            console.log(tokenizer.getCurrentStringValue());
-            outputElement.textContent = tokenizer.getCurrentStringValue();
-        } catch (err) {
-            console.error(err);
-            outputElement.innerHTML = `<span style="background:red;">${err}</span>`;
+    
+    calculatorUI.addEventListener("click", (event) => {
+        const target = event.target,
+            dataset = target instanceof HTMLElement ? target.dataset : null,
+            key = dataset?.key;
+        
+        if (key) {
+            calculatorEngine.serialize(key);
+            inputElement.value = calculatorEngine.getCurrentStringValue();
         }
     });
+
+//     calculatorUI.addEventListener("keypress", (event) => {
+//         event.preventDefault();
+//         inputElement.value = event.key;
+//     });
+
+//     let inputElement = document.getElementById('calculator__input');
+//     let outputElement = document.getElementById('calculator__result');
+
+//     if (inputElement === null) {
+//         return;
+//     }
+//     inputElement.focus();
+//     inputElement.addEventListener('keypress', event => {
+//         if (outputElement === null) {
+//             return;
+//         }
+//         try {
+//             calculatorEngine.serialize(event.key);
+//             console.log(calculatorEngine.getCurrentStringValue());
+//             outputElement.textContent = calculatorEngine.getCurrentStringValue();
+//         } catch (err) {
+//             console.error(err);
+//             outputElement.innerHTML = `<span style="background:red;">${err}</span>`;
+//         }
+//     });
+
 }
 
 main();
