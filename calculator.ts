3c4c125e408d3563d42c14b5357ff67492fd7ce9
Noam Anisfeld <NoamAnisfeld@gmail.com>
2022-09-20 12:59:37 +0300

    cleanup

--- calculator.ts
+++ calculator.ts
@@ -1,37 +1,20 @@
 import { CalculatorEngine } from "./calculator-engine.js";
 
-const DEBUG = true;
+const DEVELOPMENT_MODE = true;
 
-if (DEBUG) {
-    document.documentElement.addEventListener('keypress', (event) => {
-        if (event.key === 'z' || event.key === 'Z') {
+if (DEVELOPMENT_MODE) {
+    document.documentElement.addEventListener('keyup', (event) => {
+        if (event.code === 'KeyZ') {
             // @ts-ignore
             location.reload(true /* bypass catch on Firefox */);
         }
     });
 }
 
-function validatePrototype<T>(object:T, prototype:Function): T | null {
-    if (object instanceof prototype) {
-        return object;
-    } else {
-        return null;
-    }
-}
-
 function main(): void {
     const calculatorEngine = new CalculatorEngine,
         calculatorUI = document.querySelector(".calculator-wrapper"),
-	    inputElement:HTMLInputElement | null | undefined = 
-            validatePrototype(
-                calculatorUI?.querySelector('input[type="text"]'),
-                HTMLInputElement
-            ),
-        yairModeSwitch:HTMLInputElement | null =
-            validatePrototype(
-                document.querySelector('input#yair-mode'),
-                HTMLInputElement
-            );
+	    inputElement = calculatorUI?.querySelector('input[type="text"]') as HTMLInputElement;
 
     if (!calculatorUI || !inputElement) {
         return;
@@ -39,45 +22,15 @@ function main(): void {
     
     calculatorUI.addEventListener("click", (event) => {
         const target = event.target,
-            dataset = target instanceof HTMLElement ? target.dataset : null,
-            key = dataset?.key;
-        
-        if (key) {
-            calculatorEngine.serialize(key);
-            if (yairModeSwitch?.checked && key !== '=' && key !== 'C') {
-                inputElement.value += key;
-            } else {
-                inputElement.value = calculatorEngine.getCurrentStringValue();
-            }
+            key = (target as HTMLElement)?.dataset.key;
+
+        if (!key) {
+            return;
         }
+
+        calculatorEngine.serialize(key);
+        inputElement.value = calculatorEngine.getCurrentStringValue();
     });
-
-//     calculatorUI.addEventListener("keypress", (event) => {
-//         event.preventDefault();
-//         inputElement.value = event.key;
-//     });
-
-//     let inputElement = document.getElementById('calculator__input');
-//     let outputElement = document.getElementById('calculator__result');
-
-//     if (inputElement === null) {
-//         return;
-//     }
-//     inputElement.focus();
-//     inputElement.addEventListener('keypress', event => {
-//         if (outputElement === null) {
-//             return;
-//         }
-//         try {
-//             calculatorEngine.serialize(event.key);
-//             console.log(calculatorEngine.getCurrentStringValue());
-//             outputElement.textContent = calculatorEngine.getCurrentStringValue();
-//         } catch (err) {
-//             console.error(err);
-//             outputElement.innerHTML = `<span style="background:red;">${err}</span>`;
-//         }
-//     });
-
 }
 
 main();
