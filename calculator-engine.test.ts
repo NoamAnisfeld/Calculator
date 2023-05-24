3b92c7202b6e880271777428ad400c32df9455f1
Noam Anisfeld <NoamAnisfeld@gmail.com>
2023-05-25 00:23:09 +0300

    adding calculator-engine.test.ts

---
+++ calculator-engine.test.ts
@@ -0,0 +1,34 @@
+import { CalculatorEngine } from './calculator-engine';
+
+describe('single value inputs', () => {
+    let calculatorEngine: CalculatorEngine;
+
+    beforeEach(() => {
+        calculatorEngine = new CalculatorEngine;
+    });
+
+    function testPattern(input: string[], expectedOutput: string) {
+        input.forEach(char => calculatorEngine.serialize(char));
+        expect(calculatorEngine.getCurrentStringValue()).toBe(expectedOutput);
+    }
+
+    test('simple number', () => {
+        testPattern(['4', '6', '1'], '461');
+    });
+
+    test('decimal number', () => {
+        testPattern(['4', '.', '6', '1'], '4.61');
+    });
+
+    test('decimal number with implicit zero', () => {
+        testPattern(['.', '3', '5'], '0.35');
+    });
+
+    test('ignores leading zeroes', () => {
+        testPattern(['0', '3'], '3');
+    });
+
+    test('ignores multiple zeroes', () => {
+        testPattern(['0', '0', '0'], '0');
+    });
+});
